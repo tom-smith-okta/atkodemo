703750a10f964b58339db1224100c2138e552443
Tom Smith <tom.smith@okta.com>
2016-11-12 09:03:22 -0500

    better formatting for settings

--- settings.php
+++ settings.php
@@ -7,4 +7,19 @@ $thisPage = new htmlPage($config);
 // let's not show our api key in the browser.
 $config["apiKey"] = substr($config["apiKey"], 0, 5) . "...";
 
-echo json_encode($config, JSON_PRETTY_PRINT);
+// echo json_encode($config, JSON_PRETTY_PRINT);
+
+foreach ($config as $key => $value) {
+
+	if (is_array($value)) {
+		echo "<p>" . $key . ": " . json_encode($value) . "</p>";
+	}
+	else {
+		// echo "<p>" . $key . ": <xmp>" . $value . "</xmp>";
+
+		echo "<xmp>" . $key . ": " . $value . "</xmp>";
+
+	}
+
+	echo "<br>";
+}
